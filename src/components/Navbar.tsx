c736ff02cbeeb21540f102b5461f3b98a08001b4
se7enseal <se7enseal@hotmail.com>
2026-04-29 11:56:31 +0800

    Fix logo display with wrapper div

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,9 @@ export default function Navbar() {
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center gap-2">
-            <img src="/logo.png" alt="365比分网" className="h-10 w-auto bg-black rounded object-contain" />
+            <div className="h-10 w-auto bg-black rounded overflow-hidden">
+              <img src="/logo.png" alt="365比分网" className="h-full w-full object-contain" />
+            </div>
             <span className="text-xl font-bold text-white tracking-wide">
               365比分网
             </span>
